f026f3d2b2ab4aad4ba9529ec5dc012cfa45cc11
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-09 15:11:08 -0500

    initial commit to while loop

---
+++ while.php
@@ -0,0 +1,17 @@
+<?php
+/*
+CodeUp Day 4
+Dev: Frank Pigeon
+Date: May 8, 2014
+*/
+
+//variables
+$test = 5;
+
+while ($test <= 15) {
+	echo "The value of \$test is $test\n";
+	$test++;
+} //end of while
+
+
+?>

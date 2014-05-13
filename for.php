e9d1a198fcbd10222b4295402fecba0fa8f4bb1c
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-13 11:06:17 -0500

    default incrementor to 1 if no input

--- for.php
+++ for.php
@@ -21,12 +21,18 @@ $end_num = (int)fgets(STDIN);
 echo "Please enter an incrementor: ";
 $incrementor = (int)fgets(STDIN);
 
+//default incrementor = 1 if no input
+if ($incrementor == "\n"){
+	$incrementor = 1; //default incrementor
+}//end of default incrementor
+
 //for debugging; remove later
 echo ("\$start_num is $start_num and \$end_num is $end_num\n");
 echo ("\$incrementor is $incrementor\n");
 
 //pass on value to index
 $index = $start_num;
+
 //for loop
 for ($index; $index <= $end_num; $index+=$incrementor) {
 	echo ($index . PHP_EOL);

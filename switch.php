d1daa0510e0a9a0ee4208c9ee598968cb5a77b72
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-14 13:41:09 -0500

    created an if else that gives you the same result

--- switch.php
+++ switch.php
@@ -13,33 +13,59 @@ date_default_timezone_set('America/Chicago');
 // Get Day of Week as number
 // 1 (for Monday) through 7 (for Sunday)
 $day_of_week = date('N');
-//$day_of_week = Monday;
+//$day_of_week = 0;
 
-switch($day_of_week) {
-    case 1:
-        echo 'Today is Monday';
-        break;
-    case 2:
-        echo 'Today is Tuesday';
-    	break;
-    case 3:
-        echo 'Today is Wednesday';
-    	break;
-    case 4:
-        echo 'Today is Thursday';
-    	break;
-    case 5:
-        echo 'Today is Friday';
-    	break;
-    case 6:
-        echo 'Today is Saturday';
-    	break;
-    case 7:
-        echo 'Today is Sunday';
-    	break;
-    default:
-    	echo 'Today is an unknown day?';
-    	break;
-}//end of switch
+// switch($day_of_week) {
+//     case 1:
+//         echo 'Today is Monday';
+//         break;
+//     case 2:
+//         echo 'Today is Tuesday';
+//     	break;
+//     case 3:
+//         echo 'Today is Wednesday';
+//     	break;
+//     case 4:
+//         echo 'Today is Thursday';
+//     	break;
+//     case 5:
+//         echo 'Today is Friday';
+//     	break;
+//     case 6:
+//         echo 'Today is Saturday';
+//     	break;
+//     case 7:
+//         echo 'Today is Sunday';
+//     	break;
+//     default:
+//     	echo 'Today is an unknown day?';
+//     	break;
+// }//end of switch
+
+
+if ($day_of_week == 1){
+	echo 'Today is Monday';
+} // end of Monday
+elseif ($day_of_week == 2) {
+	echo 'Today is Tuesday';
+}//end of Tuesday
+elseif ($day_of_week == 3) {
+	echo 'Today is Wednesday';
+}//end of Wednesday
+elseif ($day_of_week == 4) {
+	echo 'Today is Thursday';
+}//end of Thursday
+elseif ($day_of_week == 5) {
+	echo 'Today is Friday';
+}//end of Friday
+elseif ($day_of_week == 6) {
+	echo 'Today is Saturday';
+}//end of Saturday
+elseif ($day_of_week == 7) {
+	echo 'Today is Sunday';
+}//end of Sunday
+else {
+	echo 'Today is unknown';
+}//end of unknown
 
 echo PHP_EOL;

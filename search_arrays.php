2c8a888efbcd80de4fd9db8ed8a32522be7ef594
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-19 13:40:45 -0500

    initial commit

---
+++ search_arrays.php
@@ -0,0 +1,19 @@
+<?php
+/*
+CodeUp
+Dev: Frank Pigeon
+Date: May 19 2014
+
+Create a function that returns TRUE or FALSE if an array value is found.
+Search for Tina and Bob in $names. Make sure it works as expected.
+
+Create a function to compare 2 arrays that returns the number of values in common between the arrays.
+ Use the 2 example arrays and make sure your solution uses array_search().
+ */
+
+// first names
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+ 

8c13c284ca3e163b7e8424b693102bc20939ab5b
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-19 13:52:20 -0500

    wrote a function to return true or false based on name searched

--- search_arrays.php
+++ search_arrays.php
@@ -11,9 +11,26 @@ Create a function to compare 2 arrays that returns the number of values in commo
  Use the 2 example arrays and make sure your solution uses array_search().
  */
 
+function array_finder($search, $array){
+	if(array_search($search, $array) !== FALSE){
+		$found = TRUE;
+	} //end of found
+	else {
+		$found = FALSE;
+	} //not found
+	
+	return $found;
+
+}//end of array_finder
+
+
 // first names
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+//function calls
+var_dump(array_finder('Tina', $names));
+var_dump(array_finder('Bob', $names));
  
+

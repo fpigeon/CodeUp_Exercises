b584190f05996c2594931f52f288ab9dbde5eda5
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-20 14:07:49 -0500

    turned into a function and get the same output

--- list_with_commas.php
+++ list_with_commas.php
@@ -2,35 +2,35 @@
 /*
 CodeUp
 Dev: Frank Pigeon
-Date: May 19 2014
-
-
- */
-
+Date: May 20 ,2014
+*/
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-  // Your solution goes here!
+function humanized_list($string) {
+	$exploded = explode(', ', $string);
+	$last_one = array_pop($exploded);
+	$last_one = 'and ' . $last_one;
+	array_push($exploded, $last_one);
+	$imploded = implode(', ', $exploded);
+	return $imploded;
 }
 
 // List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 // Humanize that list
-//$famous_fake_physicists = humanized_list($physicists_string);
+$famous_fake_physicists = humanized_list($physicists_string);
 
 //explode the string
-$famous_fake_physicists = explode(', ', $physicists_string);
-//print_r($famous_fake_physicists);
+//$famous_fake_physicists = explode(', ', $physicists_string);
 
 //break off last one and add 'and' then add to array
-$last_physicists = array_pop($famous_fake_physicists);
-$last_physicists = 'and ' . $last_physicists;
-array_push($famous_fake_physicists, $last_physicists);
-$famous_fake_physicists = implode(', ', $famous_fake_physicists);
+//$last_physicists = array_pop($famous_fake_physicists);
+//$last_physicists = 'and ' . $last_physicists;
+//array_push($famous_fake_physicists, $last_physicists);
+//$famous_fake_physicists = implode(', ', $famous_fake_physicists);
 // Output sentence
-echo "Orginal string is:\n$physicists_string\n";
-//echo "\$last_physicists is $last_physicists\n";
-//print_r($famous_fake_physicists);
+echo "Original string is:\n$physicists_string\n";
+
 echo "\n*********Formatted Output*************\n\n";
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";

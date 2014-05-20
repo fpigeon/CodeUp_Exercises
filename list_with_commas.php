a06741bdf56ec910751801fc4e627f3b5dd93b11
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-05-20 13:48:51 -0500

    initial commit using array pop and push to add and to the last item in the array

---
+++ list_with_commas.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+CodeUp
+Dev: Frank Pigeon
+Date: May 19 2014
+
+
+ */
+
+
+// Converts array into list n1, n2, ..., and n3
+function humanized_list($array) {
+  // Your solution goes here!
+}
+
+// List of famous peeps
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+// Humanize that list
+//$famous_fake_physicists = humanized_list($physicists_string);
+
+//explode the string
+$famous_fake_physicists = explode(', ', $physicists_string);
+//print_r($famous_fake_physicists);
+
+//break off last one and add 'and' then add to array
+$last_physicists = array_pop($famous_fake_physicists);
+$last_physicists = 'and ' . $last_physicists;
+array_push($famous_fake_physicists, $last_physicists);
+$famous_fake_physicists = implode(', ', $famous_fake_physicists);
+// Output sentence
+echo "Orginal string is:\n$physicists_string\n";
+//echo "\$last_physicists is $last_physicists\n";
+//print_r($famous_fake_physicists);
+echo "\n*********Formatted Output*************\n\n";
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";

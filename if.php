68b139ba35d933b22a9ab2234afdfe7736f3c698
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-05 01:15:39 -0500

    Formatting and spacing

--- if.php
+++ if.php
@@ -11,57 +11,63 @@ $b = 10;
 $c = '10';
 
 //display output of variables
-echo ("\$a is $a\n\$b is $b\n\$c is $c\n\n\n");
-
+echo '$a is ' . $a . PHP_EOL;
+echo '$b is ' . $b . PHP_EOL;
+echo '$c is ' . $c . PHP_EOL;
+echo '-------------' . PHP_EOL;
 // Add an else clause to the next two statments
-if ($a < $b) {
+if ($a < $b)
+{
     // output the appropriate result
     echo "$a is less than $b\n";
 } //end of if
-else {
+else
+{
     echo "This is the else clause\n";
 } // end of else
-    
 
-if ($b > $a) {
+if ($b > $a)
+{
     // output the appropriate result
     echo "$b is greater than $a\n";
 } //end of if
-else {
+else
+{
     echo "This is the else clause\n";
 } // end of else
 
-
 // Shorten the next 2 statements into an if/else
-if ($b >= $c) {
+if ($b >= $c)
+{
     // output the appropriate result
     echo "$b is greater than or equal to $c\n";
 } //end if
-else {
+else
+{
     // output the appropriate result
     echo "$b is less than or equal to $c\n";
 } //end else
 
-
-
-// combine the next 4 conditionals into 
+// combine the next 4 conditionals into
 // one if/else/elseif block that checks in order for:
 // identical, equal, not identical, not equal
-if ($b === $c) {
+if ($b === $c)
+{
     // output the appropriate result
     echo "$b is identical to $c\n";
 } //end of identical
-elseif ($b == $c) {
+elseif ($b == $c) 
+{
     // output the appropriate result
     echo "$b is equal (thanks PHP) to $c\n";
 }//end of equal
-elseif ($b !== $c) {
+elseif ($b !== $c) 
+{
     // output the appropriate result
     echo "$b is not identical to $c\n";
 } //end of not identical
-else {
+else 
+{
     // output the appropriate result
     echo "$b is not equal to $c\n";
 }//end of not equal
-
-?>

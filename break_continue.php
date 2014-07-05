dfe9ba43b6471f853a289f9393e42aa8f9858e43
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-04 23:45:44 -0500

    Formatting and spacing

--- break_continue.php
+++ break_continue.php
@@ -7,14 +7,13 @@ Description:
 Create a file named break_continue.php in your exercises directory. Commit and push to GitHub after each step.
 
 1. Create a for loop that shows all even numbers between 1 and 100 using continue.
-
 2. Create another for loop that counts from 1 to 100, but stops after 10 using break.
 */
-
-for ($ii = 1; $ii <=100; $ii++) {
-	echo $ii . PHP_EOL;
-	if ($ii == 10){ //stop at 10
-		break; //break out of the loop
-	} //end of even
-	
+for ($i = 1; $i <= 100; $i++)
+{
+    echo $i . PHP_EOL;
+    if ($i == 10)
+    { //stop at 10
+        break; //break out of the loop
+    } //end of even
 } // end of for loop

65f00e6b2995fff819096442d181b627e469b8ff
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-07-05 01:38:42 -0500

    Formatting and spacing

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 CodeUp
 Dev: Frank Pigeon
@@ -6,35 +6,33 @@ Date: May 20 ,2014
 */
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($string, $alpha = FALSE) {
-	$exploded = explode(', ', $string);
-	//sort alphabetical
-	if ($alpha === TRUE){
-		sort($exploded);	
-	} // end of sort alphabetical	
-	$last_one = array_pop($exploded);
-	$last_one = 'and ' . $last_one;
-	array_push($exploded, $last_one);
-	$imploded = implode(', ', $exploded);
-	return $imploded;
-}
+// if $alpha is set to TRUE will sort alphabetically
+function humanized_list($string, $alpha = FALSE)
+{
+    //turn string into an array
+    $exploded = explode(', ', $string);
+    //sort alphabetical
+    if ($alpha === TRUE)
+    {
+        sort($exploded);
+    } //end of sort alphabetical
+
+    //add a 'and ' to the last index in the array
+    $last_one = 'and '. array_pop($exploded);
+    //add to the end of the array
+    array_push($exploded, $last_one);
+    // turn back into a string
+    $imploded = implode(', ', $exploded);
+    return $imploded;
+}//end of function humanized list
 
 // List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-// Humanize that list
+// Humanize the list
 $famous_fake_physicists = humanized_list($physicists_string, TRUE);
 
-//explode the string
-//$famous_fake_physicists = explode(', ', $physicists_string);
-
-//break off last one and add 'and' then add to array
-//$last_physicists = array_pop($famous_fake_physicists);
-//$last_physicists = 'and ' . $last_physicists;
-//array_push($famous_fake_physicists, $last_physicists);
-//$famous_fake_physicists = implode(', ', $famous_fake_physicists);
-// Output sentence
+// Output
 echo "Original string is:\n$physicists_string\n";
-
-echo "\n*********Formatted Output*************\n\n";
+echo "Formatted Output:\n";
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";

1aed01e485e646e2b7762ef24bc17c4ae0f74ea1
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-08-18 22:33:30 -0500

    Added 2 new functions list_items and get_input.

--- review/todo.php
+++ review/todo.php
@@ -10,31 +10,52 @@ Update the code to allow upper and lowercase inputs from user for all menu items
 Update the program to start numbering the list with 1 instead of 0. Make sure remove still works as expected.
 */
 // Create array to hold list of todo items
-$items = array();
-
-// The loop!
-do {
+// List array items formatted for CLI
+ function list_items($items)
+ {
+    $list_string = '';
     // Iterate through list items
     foreach ($items as $key => $item) {
         // Display each item and a newline
         $key = $key + 1;
-        echo "[$key] {$item}\n";
+        $list_string .= '[' . $key . '] ' . $item . PHP_EOL;
+    }
+    return $list_string;
+ }
+
+ // Get STDIN, strip whitespace and newlines,
+ // and convert to uppercase if $upper is true
+ function get_input($input, $upper = FALSE)
+ {
+     // Return filtered STDIN input
+    if ($upper)
+    {
+        $input = strtoupper($input);
     }
 
+    return $input = trim($input);
+ }
+
+
+$items = array();
+
+// The loop!
+do {
+    echo list_items($items);
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
+    $input = get_input(fgets(STDIN), TRUE);
 
     // Check for actionable input
-    if ($input == 'N' || $input == 'n') {
+    if ($input == 'N') {
         // Ask for entry
         echo 'Enter item: ';
         // Add entry to list array
-        $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R' || $input == 'r') {
+        $items[] = get_input(fgets(STDIN));
+    } elseif ($input == 'R') {
         // Remove which item?
         echo 'Enter item number to remove: ';
         // Get array key
@@ -43,7 +64,7 @@ do {
         unset($items[$key - 1]);
     }
 // Exit when input is (Q)uit
-} while ($input != 'Q' && $input != 'q');
+} while ($input != 'Q');
 
 // Say Goodbye!
 echo "Goodbye!\n";

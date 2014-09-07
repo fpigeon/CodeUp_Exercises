ab6d8870f0d074a5b814758d6845bb77224aa438
Frank Pigeon <frank.pigeonjr@gmail.com>
2014-09-07 16:08:32 -0500

    Added foreach loop to getBreadCrumbNavigation method to display links in an ordered list.

--- lb_challenges/ts2.php
+++ lb_challenges/ts2.php
@@ -6,10 +6,33 @@ The returned HTML code should be an ordered list of links. The last entry in the
 and should show the anchor text, but should not have an anchor tag around it.
 */
 class Template {
-	private $breadcrumbs;
+	//$breadcrumbs contains an array of Webpage objects with valid values for its member variables.
+	private $breadcrumbs = [
+								'home' => '/index.html',
+								'values' => '/values.html',
+								'about' => '/about.html',
+								'contact' => '/contact.html',
+                                'connect' => '/connect.html',
+                                'current' => '/here.html'
+							];
 	public function getBreadcrumbNavigation() {
+		// return string of html code
+		// he returned HTML code should be an ordered list of links. The last entry in the array is the current page
+		// and should show the anchor text, but should not have an anchor tag around it.
+        $current_page = array_pop($this->breadcrumbs);
+        echo '<ol>' . PHP_EOL;
+		foreach ($this->breadcrumbs as $page => $breadcrumb)
+		{
+            echo '<li>' . '<a href="' . $breadcrumb . '">' . $page . '</a>'. '</li>';
+            echo PHP_EOL;
+		}
+        echo '<li>' . $current_page . '</li>' . PHP_EOL;
+        echo '</ol>'. PHP_EOL;
 	}
 }
 class Webpage {
 	public $anchorText, $myUrl;
 }
+
+$newWebPageNav = new Template();
+$newWebPageNav->getBreadcrumbNavigation();
